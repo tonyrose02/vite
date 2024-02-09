687a6b09de7eebb585e84f5ca3a1132d425387e3
J4V3 <133072334+tonyrose02@users.noreply.github.com>
2024-02-08 22:55:19 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			"@": "./src",
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {	
+				index: resolve(__dirname, './index.html'),	
+				demo: resolve(__dirname, './live-demo.html'),			
+                login: resolve(__dirname, './login.html'),
+			},
+		},
+	},
+});
